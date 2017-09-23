5ddbe46224f91124dc39fe6c068f4529fdad4b90
Sabrina Luo <luohaojing@gmail.com>
2017-09-23 17:48:20 +0800

    add middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 import rootReducer from "../reducers";
 
-const middlewares = [];
+const middlewares = [thunk];
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
 
