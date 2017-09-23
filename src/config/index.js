4dd366066464d1ff97cf171cdd784d680a8db4bd
Sabrina Luo <luohaojing@gmail.com>
2017-09-23 17:47:48 +0800

    add config

---
+++ src/config/index.js
@@ -0,0 +1,33 @@
+const config = {
+  header: {
+    nav: [
+      {
+        id: "home",
+        path: "/",
+        text: "主頁"
+      },
+      {
+        id: "news",
+        path: "/channel/news",
+        text: "新聞"
+      },
+      {
+        id: "views",
+        path: "/section/01views",
+        text: "01觀點"
+      },
+      {
+        id: "blogger",
+        path: "/aa/01",
+        text: "01博評"
+      },
+      {
+        id: "tech",
+        path: "/login",
+        text: "科技玩物"
+      }
+    ]
+  }
+};
+
+export default config;
