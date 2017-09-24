1da3e18a87e56326c48894c29b93d77e0873e241
Sabrina Luo <luohaojing@gmail.com>
2017-09-24 14:26:12 +0800

    add storage method in util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,7 @@
 export const timeoutPromise = ms =>
   new Promise(resolve => setTimeout(resolve, ms));
+
+export const getStorage = key => JSON.parse(localStorage.getItem(key));
+
+export const setStorage = (key, value) =>
+  localStorage.setItem(key, JSON.stringify(value));
