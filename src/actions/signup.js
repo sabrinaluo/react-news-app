42d565f04469bf62f80de1f9a6ff81a041be407b
Sabrina Luo <luohaojing@gmail.com>
2017-10-12 22:02:17 +0800

    refactor error message

--- src/actions/signup.js
+++ src/actions/signup.js
@@ -1,4 +1,5 @@
 import * as types from "../constants/ActionTypes";
+import * as msg from "../constants/Message";
 import { userLogin } from "./user";
 import { getStorage, setStorage } from "../utils";
 import { DB_USERS } from "../constants";
@@ -14,22 +15,18 @@ export const submitSignupForm = (username, password, password2) => {
     const isUserExists = Boolean(users[username]);
 
     if (isUserExists) {
-      return dispatch(signupError("username has been taken!"));
+      return dispatch(signupError(msg.USERNAME_IS_TAKEN));
     }
 
     const passowrdReg = new RegExp(/^[a-zA-Z0-9]{6,12}$/);
     const isValidPassword = passowrdReg.test(password);
 
     if (!isValidPassword) {
-      return dispatch(
-        signupError(
-          "password is invalid, should be 6-12 characters (number, letter) "
-        )
-      );
+      return dispatch(signupError(msg.PASSWORD_INVALID));
     }
 
     if (password !== password2) {
-      return dispatch(signupError("password should be the same"));
+      return dispatch(signupError(msg.PASSWORD_NOT_EQAUL));
     }
 
     users[username] = password;
