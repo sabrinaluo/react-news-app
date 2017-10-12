1dc26635c34112557f477d95e0cec5e030aa7fbe
Sabrina Luo <luohaojing@gmail.com>
2017-10-12 22:00:55 +0800

    add test for login

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import httpAdapter from "axios/lib/adapters/http";
 import { API_ROOT } from "./constants";
+import localStorageMock from "./__mocks__/localStorage";
 
 axios.defaults.host = API_ROOT;
 axios.defaults.adapter = httpAdapter;
+
+window.localStorage = localStorageMock;
