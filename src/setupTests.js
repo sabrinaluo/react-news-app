cb746a3ed79912911c23b7483e68db9cfd445c5b
Sabrina Luo <luohaojing@gmail.com>
2017-10-12 22:04:06 +0800

    setup test with enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import httpAdapter from "axios/lib/adapters/http";
+import Enzyme, { shallow, render, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
 import { API_ROOT } from "./constants";
 import localStorageMock from "./__mocks__/localStorage";
 
@@ -7,3 +10,14 @@ axios.defaults.host = API_ROOT;
 axios.defaults.adapter = httpAdapter;
 
 window.localStorage = localStorageMock;
+
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+// Make Enzyme functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+// Fail tests on any warning
+console.error = message => {
+  throw new Error(message);
+};
